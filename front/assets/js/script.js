5528566ae38db820043af04514655b517a2dd839
Umid <vvumid@gmail.com>
2023-12-06 15:00:34 +0400

    changes

---
+++ front/assets/js/script.js
@@ -0,0 +1,24 @@
+function updateCount() {
+    var textarea = document.getElementById("tweetText");
+    var charCount = document.getElementById("charCount");
+    var snapButton = document.getElementById("snapButton");
+
+    var remainingChars = 255 - textarea.value.length;
+
+    charCount.textContent = remainingChars;
+    
+    if(remainingChars == 255){
+        charCount.style.display = "none"
+    }else{
+        charCount.style.display = "block"
+        if (remainingChars < 0) {
+            charCount.style.color = "red";
+            snapButton.disabled = true;
+            snapButton.style.backgroundColor = "gray";
+        } else {
+            charCount.style.color = "";
+            snapButton.disabled = false;
+            snapButton.style.backgroundColor = "";
+        }
+    }
+}
